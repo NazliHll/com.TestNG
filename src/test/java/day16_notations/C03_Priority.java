cbf096801cd067bd2f976a0c9e9c54faf9dec862
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-21 22:01:26 +0300

    21.05.2023

---
+++ src/test/java/day16_notations/C03_Priority.java
@@ -0,0 +1,25 @@
+package day16_notations;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_Priority extends TestBase {
+    @Test(priority = -5)
+    public void test01() {
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getCurrentUrl());
+
+    }
+    @Test(priority = -2)
+    public void test02() {
+        driver.get("https://www.bestbuy.com");
+        System.out.println(driver.getCurrentUrl());
+
+    }
+    @Test
+    public void instaTest() {
+        driver.get("https://www.instagram.com");
+        System.out.println(driver.getCurrentUrl());
+
+    }
+}
