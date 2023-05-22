5470ecae39f74fb774222cefd93419bf0e695e0e
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-22 11:11:43 +0300

    22.05.2023

---
+++ src/test/java/day16_notations/C07_SoftAssert.java
@@ -0,0 +1,23 @@
+package day16_notations;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C07_SoftAssert extends TestBase {
+    /*
+1."https://zero.webappsecurity.com/" adresine gidin
+2.Sign in butonuna tıklayın.
+3.Login kutusuna "username" yazın.
+4.Password kutusuna "password" yazın.
+5.Sign in butonuna basın
+6.Online banking menüsü içinde Pay Bills sayfasına gidin.
+7."Purchase Foreing Currency" tuşuna basın.
+8."Currency" drop down menüsünden Eurozone'u seçin
+9.soft assert kullanarak "Eurozone (Euro)" seçildiğini test edin.
+10.soft assert kullanarak DropDown listesinin şu seçenekleri olduğunu test edin
+  */
+    @Test
+    public void test01() {
+
+    }
+}
