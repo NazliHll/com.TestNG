37ba95dba1dae3561433d74bb70feb3276690980
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-22 15:57:30 +0300

    22.05.2023

---
+++ src/test/java/tests/day17_pom/C01_YeniDriverIlkClass.java
@@ -0,0 +1,19 @@
+package tests.day17_pom;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C01_YeniDriverIlkClass {
+    @Test
+    public void test01() {
+        Driver.getDriver().get("https://www.amazon.com");
+         /*
+        Bugüne kadar TestBase class'ına extends ederek kullandığımız driver yerine bundan sonra
+        Driver class'ından kullanacağımız getDriver static methodunu kullanacağız.
+
+        Driver.getDriver()-->  in
+        driver            -->  out
+         */
+
+    }
+}
