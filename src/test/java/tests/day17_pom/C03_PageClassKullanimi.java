e913698314953ca358619223df830913f2e1e9c2
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-22 16:52:05 +0300

    22.05.2023

--- src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
+++ src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
@@ -1,11 +1,15 @@
 package tests.day17_pom;
 
+import com.github.javafaker.Faker;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.FacebookPage;
 import utilities.Driver;
 
 public class C03_PageClassKullanimi {
     @Test
     public void test01() {
+        FacebookPage facebookPage=new FacebookPage();
         /*
         1.Facebook anasayfaya gidin
         cookies kabul edin
@@ -15,5 +19,12 @@ public class C03_PageClassKullanimi {
         5.Giriş yapılamadığını test edin
          */
         Driver.getDriver().get("https://www.facebook.com");
+        Faker faker=new Faker();
+        facebookPage.mailKutusu.sendKeys(faker.internet().emailAddress());
+        facebookPage.sifreKutusu.sendKeys(faker.internet().password());
+        facebookPage.loginKutusu.click();
+        Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
+        Driver.closeDriver();
+
     }
 }
