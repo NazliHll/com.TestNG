9aa640ab31b625956c7cc0101ba0efeac6b90a4d
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-22 11:43:09 +0300

    22.05.2023

--- src/test/java/day16_notations/C07_SoftAssert.java
+++ src/test/java/day16_notations/C07_SoftAssert.java
@@ -1,11 +1,17 @@
 package day16_notations;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
+
+import java.util.List;
 
 public class C07_SoftAssert extends TestBase {
     /*
-1."https://zero.webappsecurity.com/" adresine gidin
+1."http://zero.webappsecurity.com/" adresine gidin
 2.Sign in butonuna tıklayın.
 3.Login kutusuna "username" yazın.
 4.Password kutusuna "password" yazın.
@@ -18,6 +24,34 @@ public class C07_SoftAssert extends TestBase {
   */
     @Test
     public void test01() {
+        driver.get("http://zero.webappsecurity.com/");
+        driver.findElement(By.xpath("//button[@id='signin_button']")).click();
+        WebElement loginKutusu= driver.findElement(By.xpath("//input[@id='user_login']"));
+        loginKutusu.sendKeys("username");
+        WebElement passKutu=driver.findElement(By.xpath("//input[@id='user_password']"));
+        passKutu.sendKeys("password");
+        driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+        driver.navigate().back();
+        driver.findElement(By.xpath("//strong[text()='Online Banking']")).click();
+        driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
+        driver.findElement(By.xpath("//*[text()='Purchase Foreign Currency']")).click();
+        WebElement ddo=driver.findElement(By.xpath("//select[@name='currency']"));
+        Select select=new Select(ddo);
+        select.selectByVisibleText("Eurozone (euro)");
+        SoftAssert softAssert=new SoftAssert();
+        String secilenOption=select.getFirstSelectedOption().getText();
+        String expectedOption="Eurozone (Euro)";
+        softAssert.assertEquals(secilenOption,expectedOption,"secilen option uygun degil");
+        List<WebElement> optionList=select.getOptions();
+
+
+
+
+
+
+
+
+
 
     }
 }
