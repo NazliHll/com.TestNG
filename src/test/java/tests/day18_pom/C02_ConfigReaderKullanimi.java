dce4795ac8d0853ebbcf73d3bd18a4c6c07bdf7b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-23 17:01:32 +0300

    23.05.2023

---
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
@@ -0,0 +1,20 @@
+package tests.day18_pom;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C02_ConfigReaderKullanimi {
+    @Test
+    public void test01() {
+        /*
+        1.Facebook anasayfaya gidin
+        cookies kabul edin
+        2.Kullanıcı mail kutusuna rastgele isim yazdırın
+        3.Kullanıcı şifre kutusuna rastgele bir password yazdırın
+        4.Login butonuna basın
+        5.Giriş yapılamadığını test edin
+         */
+
+    }
+}
