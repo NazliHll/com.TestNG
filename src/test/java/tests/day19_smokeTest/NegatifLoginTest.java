0ac1163a3a8b7d922f0a6b96f77cf58d1c1c7d0b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-24 17:37:47 +0300

    24.05.2023

--- src/test/java/tests/day19_smokeTest/NegatifLoginTest.java
+++ src/test/java/tests/day19_smokeTest/NegatifLoginTest.java
@@ -9,8 +9,9 @@ import utilities.Driver;
 public class NegatifLoginTest {
     BrcPage brcPage=new BrcPage();
     @Test
-    public void yanlisSifre() {
-          /*
+    public void yanlisSifre() throws InterruptedException {
+        brcPage=new BrcPage();
+        /*
     1.https://www.bluerentalcars.com/ adresine git
     2.Login butonuna bas
     3.user email : customer@bluerentalcars.com
@@ -19,6 +20,7 @@ public class NegatifLoginTest {
        değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
      */
         Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        Thread.sleep(2000);
         brcPage.ilkLoginButonu.click();
         brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
         brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcWrongPassword"));
