7db0c44d51e27609fa5bcac3e4fc0152c6f0439b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-24 09:43:27 +0300

    24.05.2023

---
+++ src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
@@ -0,0 +1,30 @@
+package tests.day19_smokeTest;
+
+import org.testng.annotations.Test;
+import pages.BrcPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class PozitifLoginTest {
+    BrcPage brcPage=new BrcPage();
+    @Test
+    public void positiveLoginTest() {
+        /*
+    1.https://www.bluerentalcars.com/ adresine git
+    2.Login butonuna bas
+    3.user email : customer@bluerentalcars.com
+        password : 12345
+        login butonuna tıklayın
+       değerleri girildiğinde sayfaya başarılı bir şekilde giriş yap
+     */
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        brcPage.ilkLoginButonu.click();
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+        brcPage.ikinciLoginButonu.click();
+
+
+
+
+    }
+}
